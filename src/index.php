e9e3060b890928a8d7a681fc39021ecd4722670d
LucasSarri <lucassarri@hotmail.com>
2026-04-19 17:03:52 -0300

    atualizando templates do cabeçalho e footer, ajustando home e corrigindo CSS

--- src/index.php
+++ src/index.php
@@ -2,7 +2,42 @@
     include_once("templates/header.php")
 ?>
 
+<div id="main-banner">
+    <h1>Faça seu Pedido</h1>
+</div>
 
+<div id="maincontainer">
+    <div id="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Monte a pizza como desejar:</h2>
+                <form action="process/pizza.php" method="POST" id="pizza-form">
+                    <div class="form-group">
+                        <label for="borda">Borda:</label>
+                        <select name="borda" id="borda" class="form-control">
+                            <option value="">Selecione a borda</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="massa">Massa:</label>
+                        <select name="massa" id="massa" class="form-control">
+                            <option value="">Selecione a massa</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="sabores">Sabores: (Máximo 3)</label>
+                        <select multiple name="sabores[]" id="sabores" class="form-control">
+                        
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Faça seu Pedido!">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
     include_once("templates/footer.php")
